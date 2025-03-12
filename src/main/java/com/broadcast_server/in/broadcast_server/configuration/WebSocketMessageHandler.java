5038591f7d781a37ea43cd337875fd6122caa335
JP <jothiprakash888@gmail.com>
2025-03-12 18:30:16 +0530

    response mapped to nonjson handling

--- src/main/java/com/broadcast_server/in/broadcast_server/configuration/WebSocketMessageHandler.java
+++ src/main/java/com/broadcast_server/in/broadcast_server/configuration/WebSocketMessageHandler.java
@@ -5,33 +5,47 @@ import org.springframework.web.socket.*;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 import java.util.Set;
 import java.util.concurrent.CopyOnWriteArraySet;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Map;
 
 @Component
 public class WebSocketMessageHandler extends TextWebSocketHandler {
 
     private static final Set<WebSocketSession> sessions = new CopyOnWriteArraySet<>();
+    private static final ObjectMapper objectMapper = new ObjectMapper(); // JSON Serializer
 
     @Override
-    public void afterConnectionEstablished(WebSocketSession session) {
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         sessions.add(session);
         System.out.println("New client connected: " + session.getId());
+
+        // Send JSON message for user joining
+        broadcastMessage(Map.of("type", "join", "user", "User-" + session.getId()));
     }
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         System.out.println("Received message: " + message.getPayload());
 
-        // Broadcast message to all connected clients
-        for (WebSocketSession s : sessions) {
-            if (s.isOpen()) {
-                s.sendMessage(new TextMessage("Client " + session.getId() + ": " + message.getPayload()));
-            }
-        }
+        // Send JSON formatted message
+        broadcastMessage(Map.of("type", "message", "user", "User-" + session.getId(), "text", message.getPayload()));
     }
 
     @Override
-    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) {
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
         sessions.remove(session);
         System.out.println("Client disconnected: " + session.getId());
+
+        // Send JSON message for user leaving
+        broadcastMessage(Map.of("type", "leave", "user", "User-" + session.getId()));
+    }
+
+    private void broadcastMessage(Map<String, String> messageData) throws Exception {
+        String jsonMessage = objectMapper.writeValueAsString(messageData);
+        for (WebSocketSession s : sessions) {
+            if (s.isOpen()) {
+                s.sendMessage(new TextMessage(jsonMessage));
+            }
+        }
     }
 }
